a87951822d5c372608a0c406bb68933c5672af61
jslusser <jslusser@gmail.com>
2017-07-09 07:49:47 -0400

    Initial commit

---
+++ Project21/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Project21
+//
+//  Created by James Slusser on 6/29/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//  https://www.hackingwithswift.com/read/21/overview
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
+    }
+    
+    func scheduleLocal() {
+    }
+
+}
+
